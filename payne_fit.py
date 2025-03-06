70cfaa99bd1536cc081730170544234c30905d3d
Korboal <fifthrom@gmail.com>
2025-03-06 11:49:35 +0100

    kovalev

--- payne_fit.py
+++ payne_fit.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 import pandas as pd
 import scipy
-from The_Payne import utils
 from The_Payne import spectral_model
-from The_Payne import fitting
 from convolve_vmac import *
 from scipy.interpolate import interp1d
 from scipy.optimize import curve_fit
@@ -58,24 +56,34 @@ def make_model_spectrum_for_curve_fit(payne_coeffs, wavelength_payne, resolution
 
         wavelength_payne_ = wavelength_payne
 
+        if vmac > 1e-2:
+            wavelength_payne_, spec_payne = conv_macroturbulence(wavelength_payne_, spec_payne, vmac)
+        if vrot > 1e-2:
+            wavelength_payne_, spec_payne = conv_rotation(wavelength_payne_, spec_payne, vrot)
         if resolution_val is not None:
             wavelength_payne_, spec_payne = conv_res(wavelength_payne_, spec_payne, resolution_val)
-        #if vrot > 0:
-            #wavelength_payne_, spec_payne = conv_rotation(wavelength_payne_, spec_payne, vrot)
-        if vmac > 0:
-            wavelength_payne_, spec_payne = conv_macroturbulence(wavelength_payne_, spec_payne, vmac)
+
+        wavelength_payne_ = wavelength_payne_ * (1 + (doppler_shift / 299792.))
 
         f_interp = interp1d(
             wavelength_payne_,
             spec_payne,
             kind='linear',
             bounds_error=False,
-            fill_value='extrapolate'
+            fill_value=1
         )
 
-        wavelength_obs = wavelength_obs / (1 + (doppler_shift / 299792.))
+        interpolated_spectrum = f_interp(wavelength_obs)
 
-        return f_interp(wavelength_obs)
+        #plt.scatter(wavelength_obs, flux_obs, s=3, color='k')
+        #plt.plot(wavelength_obs, interpolated_spectrum, color='r')
+        #plt.show()
+
+        # calculate chi-squared
+        #chi_squared = np.sum((interpolated_spectrum - flux_obs) ** 2)
+        #print(chi_squared)
+
+        return interpolated_spectrum
 
     return model_spectrum_for_curve_fit
 
@@ -83,6 +91,8 @@ def make_model_spectrum_for_curve_fit(payne_coeffs, wavelength_payne, resolution
 if __name__ == '__main__':
     path_model = "/Users/storm/PycharmProjects/payne/test_network/payne_alt_smallerldelta_ts_nlte_lesselements_hr10_2025-02-27-08-43-08.npz"
     payne_coeffs, wavelength_payne, labels = load_payne(path_model)
+
+    resolution_val = None
 
     real_labels = [5.777, 4.44, 0.0, 1.0, 0., 0., 0., 0, 0]
     scaled_labels = (
@@ -96,15 +106,32 @@ if __name__ == '__main__':
         kovalev_alt=True
     )
 
-    #wavelength_obs, flux_obs = np.loadtxt("/Users/storm/PhD_2022-2025/Spectra/Sun/KPNO_FTS_flux_2960_13000_Kurucz1984.txt", dtype=float, unpack=True)
+    wavelength_obs, flux_obs = np.loadtxt("/Users/storm/PhD_2022-2025/Spectra/Sun/KPNO_FTS_flux_2960_13000_Kurucz1984.txt", dtype=float, unpack=True)
+    wavelength_obs, flux_obs = np.loadtxt("./ts_spectra/sun_nlte.spec", dtype=float, unpack=True, usecols=(0, 1))
     wavelength_obs, flux_obs = np.loadtxt("/Users/storm/PycharmProjects/4most/Victor/spectra_victor_jan25/G48-29", dtype=float, unpack=True)
-    resolution_val = 20_000
+    data = np.loadtxt("18Sco_corrected.txt", dtype=float, unpack=True)
+    wavelength_obs, flux_obs = data[:, 0], data[:, 1]
+    resolution_val = None
 
     l_cut = (wavelength_obs > wavelength_payne[0]) & (wavelength_obs < wavelength_payne[-1])
     wavelength_obs = wavelength_obs[l_cut]
     flux_obs = flux_obs[l_cut]
 
-    p0 = [4.777, 4.44, -2.0, 1.5, -1., -1., -1., -1, -1, 1, 1, 1]
+    p0 = [7.777, 2.44, -3.0, 1.5, -2., -2., -2., -2, -2, 0, 8, 0]
+
+    def_bounds = ([3.5, 0, -4, 0.5, -3, -3, -3, -3, -3, 0, 0, -20], [8, 5, 0.5, 3, 3, 3, 3, 3, 3, 15, 15, 20])
+
+    input_values = [None] * len(p0)
+    #input_values = (6.394, 4.4297, -2.8919, 1.4783, None, None, None, None, None, 3.7822, 0, 0)
+    #input_values = (4287.7906, 4.5535, -0.5972, 0.6142, None, None, None, None, None, 5.399, 0, 0)
+    #input_values = (6290.449, 4.6668, -3.7677, 1.1195, None, None, None, None, None, 1.2229, 0, 0)
+    for i, input_value in enumerate(input_values):
+        if input_value is not None:
+            if i == 0 and input_value > 100:
+                input_value /= 1000
+            p0[i] = input_value
+            def_bounds[0][i] = input_value - 1e-3
+            def_bounds[1][i] = input_value + 1e-3
 
     model_func = make_model_spectrum_for_curve_fit(
         payne_coeffs,
@@ -117,18 +144,20 @@ if __name__ == '__main__':
         wavelength_obs,
         flux_obs,
         p0=p0,
-        bounds=([3.5, 0, -4, 0.5, -3, -3, -3, -3, -3, 0, 0, -10], [8, 5, 0.5, 3, 3, 3, 3, 3, 3, 15, 15, 10])
+        bounds=def_bounds,
     )
 
-    print(popt)
+    #print(popt)
     labels.append('vrot')
     labels.append('vmac')
     labels.append('doppler_shift')
     for label, value in zip(labels, popt):
         if label != 'teff':
-            print(f"{label:<15}: {value:>10.3f}")
+            print(f"{label:<15}: {value:>10.3f} +/- {np.sqrt(np.diag(pcov))[labels.index(label)]:>10.3f}")
         else:
-            print(f"{label:<15}: {value*1000:>10.3f}")
+            print(f"{label:<15}: {value*1000:>10.3f} +/- {np.sqrt(np.diag(pcov))[labels.index(label)]*1000:>10.3f}")
+    if resolution_val is not None:
+        print(f"{'Resolution':<15}: {int(resolution_val):>10}")
 
     doppler_shift = popt[-1]
     vmac = popt[-2]
@@ -140,14 +169,16 @@ if __name__ == '__main__':
                                                                   NN_coeffs=payne_coeffs, kovalev_alt=True)
 
     wavelength_payne_plot = wavelength_payne
+    if vmac > 1e-3:
+        wavelength_payne_plot, payne_fitted_spectra = conv_macroturbulence(wavelength_payne_plot, payne_fitted_spectra, vmac)
+    if vrot > 1e-3:
+        wavelength_payne_plot, payne_fitted_spectra = conv_rotation(wavelength_payne_plot, payne_fitted_spectra, vrot)
     if resolution_val is not None:
         wavelength_payne_plot, payne_fitted_spectra = conv_res(wavelength_payne_plot, payne_fitted_spectra, resolution_val)
-    # if vrot > 0:
-    # wavelength_payne_plot, payne_fitted_spectra = conv_rotation(wavelength_payne_plot, payne_fitted_spectra, vrot)
-    if vmac > 0:
-        wavelength_payne_plot, payne_fitted_spectra = conv_macroturbulence(wavelength_payne_plot, payne_fitted_spectra, vmac)
 
     plt.figure(figsize=(18, 6))
-    plt.scatter(wavelength_obs / (1 + (doppler_shift / 299792.)), flux_obs, label="Observed", s=3, color='k')
-    plt.plot(wavelength_payne_plot, payne_fitted_spectra, label="Payne", color='r')
+    plt.scatter(wavelength_obs, flux_obs, label="Observed", s=3, color='k')
+    plt.plot(wavelength_payne_plot * (1 + (doppler_shift / 299792.)), payne_fitted_spectra, label="Payne", color='r')
+    plt.ylim(0.0, 1.05)
+    plt.xlim(wavelength_payne_plot[0], wavelength_payne_plot[-1])
     plt.show()
